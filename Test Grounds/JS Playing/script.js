b61af4ead6683b0656098e7c20cea70ce5ddf352
ryan <35403012+Rxntly@users.noreply.github.com>
2024-07-31 22:52:41 +0300

    Add files via upload

---
+++ Test Grounds/JS Playing/script.js
@@ -0,0 +1,48 @@
+// const balance = '2000';
+// const description = 'Salary';
+// const newTransaction = true;
+// const expenses = [100,200,300];
+
+// document.querySelector('.balance-number');
+
+// const balanceNumberEl = document.querySelector('.balance-number');
+// balanceNumberEl.textContent = balance;
+
+
+// console.log(1000);
+
+
+// //object
+// const transaction = {
+//    description: 'Salary',
+//    amount: 5000
+// }
+
+// console.log(transaction);
+
+// // function to use later
+
+// function calculateBalance(){
+//    const income = 5000;
+//     const expenses = 2000;
+//     const finalBalance = income-expenses;
+//     console.log(finalBalance);
+// }
+
+// calculateBalance();
+
+// function calculatingX(x1, x2){
+//    const totalX = x1 + x2;
+//    return totalX;
+// }
+
+// const result = calculatingX(100,200);
+// console.log(result);
+
+
+const balanceE = document.querySelector('.balance-number');
+
+balanceE.addEventListener("click", function(){
+balanceE.style.color = "red";
+})
+
